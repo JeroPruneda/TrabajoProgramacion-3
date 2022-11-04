15bacc956e0f461d1a60abecd71e76c2f2b7b6ec
josesoutou <josesoutou@gmail.com>
2022-11-03 23:08:44 -0300

    cerrar la sesion del usuario

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,7 +1,8 @@
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
+import { Text, View, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import Home from '../screens/Home/Home'
+import Profile from '../screens/Profile/Profile'
 import {FontAwesome} from "@expo/vector-icons"
 const Tab = createBottomTabNavigator()
 import React from 'react'
@@ -11,6 +12,7 @@ export default function TabNavigation() {
   return (
     <Tab.Navigator>
         <Tab.Screen name='Home' component={Home} options= {{tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />}}/>
+        <Tab.Screen name='Profile' component={Profile} options= {{tabBarIcon: () => <FontAwesome name="perfil" size={24} color="black" />}}/>
        
 
     </Tab.Navigator>
